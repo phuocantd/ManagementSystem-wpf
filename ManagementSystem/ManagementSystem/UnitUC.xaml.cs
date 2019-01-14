c4611a12da546110885bd4e83b7053c808ca60ec
phuoc_an <phuocantd@gmail.com>
2019-01-15 01:24:06 +0700

    thêm xóa sửa Unit

--- ManagementSystem/ManagementSystem/UnitUC.xaml.cs
+++ ManagementSystem/ManagementSystem/UnitUC.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,70 @@ namespace ManagementSystem
     /// </summary>
     public partial class UnitUC : UserControl
     {
+        public static DataGrid dtaGrid;
+        int isUpdate = 0;
+        Unit tmp;
+
         public UnitUC()
         {
             InitializeComponent();
+            Load();
+        }
+
+        private void Load()
+        {
+            UnitDataGrid.ItemsSource = DataProvider.Ins.DB.Units.ToList();
+            dtaGrid = UnitDataGrid;
+        }
+
+        private void btn_Add(object sender, RoutedEventArgs e)
+        {
+            Unit newUnit = new Unit() { DisplayName=UnitDisplayName.Text };
+            DataProvider.Ins.DB.Units.Add(newUnit);
+            DataProvider.Ins.DB.SaveChanges();
+            Load();
+        }
+
+        private void btn_Edit(object sender, RoutedEventArgs e)
+        {
+            if (isUpdate != 0)
+            {
+                int ID = tmp.ID;
+                Unit updateUnit = (from m in DataProvider.Ins.DB.Units
+                                   where m.ID == ID
+                                   select m).Single();
+                updateUnit.DisplayName = UnitDisplayName.Text;
+
+                DataProvider.Ins.DB.SaveChanges();
+                Load();
+                isUpdate = 0;
+            }
+            
+        }
+
+        private void btn_Del(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int ID = (UnitDataGrid.SelectedItem as Unit).ID;
+                var deleteUnit = DataProvider.Ins.DB.Units.Where(m => m.ID == ID).Single();
+                DataProvider.Ins.DB.Units.Remove(deleteUnit);
+                DataProvider.Ins.DB.SaveChanges();
+                Load();
+            }
+            catch{
+
+            }
+
+            
+        }
+
+
+        private void mouseLeft(object sender, MouseButtonEventArgs e)
+        {
+            tmp = (UnitDataGrid.SelectedItem as Unit);
+            UnitDisplayName.Text = tmp.DisplayName;
+            isUpdate = 1;
         }
     }
 }
