007dd367806c022f431f714c7ba50da456c82d46
phuoc_an <phuocantd@gmail.com>
2019-01-14 19:56:05 +0700

    add dataprovider

---
+++ ManagementSystem/ManagementSystem/DataProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementSystem
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+
+        public QuanLyBanHangEntities DB { get; set; }
+
+        private DataProvider()
+        {
+            DB = new QuanLyBanHangEntities();
+        }
+    }
+}
